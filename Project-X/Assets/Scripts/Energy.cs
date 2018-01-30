8d6f05d3300133e8958d4cbaa813bde8b65c6779
LarssonSv <gizziy@hotmail.com>
2018-01-30 14:10:00 +0100

    UI for main scene

---
+++ Project-X/Assets/Scripts/Energy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour {
+
+    [SerializeField]
+    Image energyBar;
+
+    float energy;
+
+    private void Start() {
+        energy = 100;
+        InvokeRepeating("DecayEnergy", 1f, 5f);
+    }
+
+    private void Update() {
+        energyBar.fillAmount = energy / 100;
+    }
+
+    private void DecayEnergy () {
+        energy -= 1;
+        Debug.Log("energy: " + energy);
+    }
+}
---
+++ Project-X/Assets/Scripts/Fuel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fuel : MonoBehaviour {
+
+    [SerializeField]
+    Image bar;
+
+    float fuel;
+
+    private void Start() {
+        fuel = 50;
+    }
+
+    private void Update() {
+        bar.fillAmount = fuel / 100;
+    }
+
+}
