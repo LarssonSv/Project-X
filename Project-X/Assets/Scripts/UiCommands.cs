45fa0c57e64d94bf1c05fae97342d7de0bceefe7
LarssonSv <gizziy@hotmail.com>
2018-01-30 12:33:18 +0100

    Ui Commands

---
+++ Project-X/Assets/Scripts/UiCommands.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UiCommands : MonoBehaviour {
+
+    bool toggle = true;
+
+    [SerializeField]
+    GameObject options;
+    Slider sliderAudio;
+
+    public void LoadSceneByIndex (int x) {
+        SceneManager.LoadScene(x);
+        Debug.Log("Loaded scene:" + x);
+    }
+
+    public void Exit() {
+        Application.Quit();
+        Debug.Log("Does not work in editor, works when running from EXE");
+    }
+
+    public void ToggleScreen (){
+        options.SetActive(toggle);
+        toggle = !toggle;
+    }
+
+    public void VolumeUpdate() {
+        PlayerPrefs.SetFloat("volume", sliderAudio.value);
+    }
+}
