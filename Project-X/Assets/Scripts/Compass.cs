b6e290f4618b2b50d2e3bd29965a7bc38246484d
Simon Larsson <gizziy@hotmail.com>
2018-01-31 21:34:32 +0100

    Compass v1

---
+++ Project-X/Assets/Scripts/Compass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour {
+
+    public Vector3 northDirection;
+    public Transform player;
+    public Quaternion MissionDirection;
+
+
+    public RectTransform northLayer;
+    public RectTransform MissionLayer;
+
+    public Transform missionplace;
+
+
+    void Update () {
+        ChangeNorthDirection();
+        ChangeMissionDirection();
+
+    }
+
+    void ChangeNorthDirection() {
+        northDirection.z = player.eulerAngles.z;
+        northLayer.localEulerAngles = northDirection;
+    }
+    
+    void ChangeMissionDirection() {
+        Vector3 dir = transform.position - missionplace.position;
+        MissionDirection = Quaternion.LookRotation(dir);
+
+        MissionDirection.z = -MissionDirection.y;
+        MissionDirection.x = 0;
+        MissionDirection.y = 0;
+
+        MissionLayer.localRotation = MissionDirection * Quaternion.Euler(northDirection);
+    }
+
+
+
+}
