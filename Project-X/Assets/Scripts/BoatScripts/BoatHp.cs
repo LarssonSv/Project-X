1fbcce4145e8d0831a97e3da1a24f74aaf6d2ff0
AlexanderMalthed <34477934+AlexanderMalthed@users.noreply.github.com>
2018-02-02 15:09:03 +0100

    BoatHp + Islands

---
+++ Project-X/Assets/Scripts/BoatScripts/BoatHp.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatHp : MonoBehaviour {
+
+    [SerializeField]
+    private int hp, damageAmount;
+
+    int GetHp()
+    {
+        return hp;
+    }
+
+    void Damage(int damage)
+    {
+        hp -= damage;
+        if (hp < 0)
+        {
+            hp = 0;
+            Destroy(gameObject);
+            //Instansiate playerCharacter at previousLocation
+            Debug.Log("Boat destroyed, you were knocked out and awoke on the last island you visited.");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Danger")
+        {
+            Damage(damageAmount);
+        }      
+    }
+}
