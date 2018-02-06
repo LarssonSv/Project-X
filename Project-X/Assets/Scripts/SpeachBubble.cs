bc6985a81e76e7e98ec52a81104cec3e3f595f99
LarssonSv <gizziy@hotmail.com>
2018-02-06 12:17:44 +0100

    SpeachBubble v1

---
+++ Project-X/Assets/Scripts/SpeachBubble.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeachBubble : MonoBehaviour {
+
+    [SerializeField]
+    Canvas speachBubblePrefab;
+
+    private Canvas clone;
+
+    public void OpenBubble() {
+
+        if (clone == null) {
+            clone = Instantiate(speachBubblePrefab);
+            
+        }
+        else {
+
+            Destroy(clone.gameObject);
+
+        }
+
+    }
+
+    public void FeedCanvas(string displayName, string speach) {
+
+        Text name = GameObject.FindGameObjectWithTag("speachName").GetComponent<Text>();
+        name.text = displayName;
+
+        Text mainText = GameObject.FindGameObjectWithTag("speachText").GetComponent<Text>();
+        mainText.text = speach;
+    }
+
+}
