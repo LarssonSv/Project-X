49a2d14054a9f8474afc338b94881901b940f8fc
AlexanderMalthed <34477934+AlexanderMalthed@users.noreply.github.com>
2018-02-08 16:48:45 +0100

    Revert "Merge branch 'master' of https://github.com/LarssonSv/Project-X"
    This reverts commit 921a39f67ec10f635ac516ee51515c0413733fb1, reversing
    changes made to 167accd604c8200295b260333303a1127ae88236.

--- Project-X/Assets/Scripts/BoatScripts/BoatHp.cs
+++ Project-X/Assets/Scripts/BoatScripts/BoatHp.cs
@@ -8,39 +8,13 @@ public class BoatHp : MonoBehaviour {
     [SerializeField]
     private int startHp, damageAmount;
     private int hp;
-    [SerializeField]
     private Image bar;
-    private List<Vector3> lastPos;
-    private int i = 0;
-    private bool knockback = false;
 
     private void Start()
     {
-        lastPos = new List<Vector3>();
         hp = startHp;
         bar = GameObject.FindGameObjectWithTag("HpBar").GetComponent<Image>();
         bar.fillAmount = hp * 0.01f;
-    }
-
-    private void Update() 
-    {
-        if (!knockback) 
-        {
-            lastPos.Insert(0, transform.position);
-            if (lastPos.Count > Mathf.Round(0.5f / Time.fixedDeltaTime))
-            {
-                lastPos.RemoveAt(lastPos.Count -1);
-            }
-        }
-        else if (lastPos.Count > 0) 
-        {
-            transform.position = lastPos[0];
-            lastPos.RemoveAt(0);
-        }
-        else
-        {
-            knockback = false;
-        }
     }
 
     public void RestoreHp()
@@ -69,14 +43,12 @@ public class BoatHp : MonoBehaviour {
             GameObject.FindGameObjectWithTag("FuelBar").GetComponent<Image>().fillAmount = 0.0f;
             Debug.Log("Boat destroyed, you were knocked out and awoke on the last island you visited.");
         }
-       // transform.position = Vector3.Lerp(transform.position, lastPos[99].position, 1f * Time.deltaTime);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Danger")
         {
-            knockback = true;
             Damage(damageAmount);
         }      
     }
